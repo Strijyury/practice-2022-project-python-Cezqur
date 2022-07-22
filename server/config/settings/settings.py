8c77d7589a393f5383bae9b2bd0e0e412a25edca
cezqur <strizhov.yura97@gmail.com>
2022-07-22 11:47:04 +0700

    Добавление ROTATE_REFRESH_TOKENS: True Изменения в poetry.lock, pyproject.toml

--- server/config/settings/settings.py
+++ server/config/settings/settings.py
@@ -145,6 +145,7 @@ REST_FRAMEWORK = {
 SIMPLE_JWT = {
     'ACCESS_TOKEN_LIFETIME': timedelta(minutes=1),
     'REFRESH_TOKEN_LIFETIME': timedelta(days=30),
+    'ROTATE_REFRESH_TOKENS': True,
 }
 
 ##################################################################
